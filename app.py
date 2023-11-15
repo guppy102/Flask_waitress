67528ca2813810b243fbc26ba2a8e8109d0e7630
guppy102 <211k10176@rissho-univ.jp>
2023-11-15 14:13:33 +0900

    threadsを指定して適切なアプリ稼働を目指します

--- app.py
+++ app.py
@@ -322,7 +322,7 @@ if __name__ == "__main__":
     #app.run('0.0.0.0', port=5000)
     
     #app.run('0.0.0.0',port=5000)
-    serve(app, host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000,threads=8)
     
     #本番
     #serve(app, host='0.0.0.0', port=80)
